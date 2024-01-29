0a6505334b11837725ddfbf06fcce00c7e0d389a
mas0110 <masoudmohammed13@gmail.com>
2024-01-29 23:56:29 +0100

    file html js e css

---
+++ main.js
@@ -0,0 +1,13 @@
+const { createApp  } = Vue 
+
+    createApp({
+        data() {
+            return {
+                // array di oggetti con contenuti del carosello
+
+            }
+        },
+        methods:{
+
+        },
+    }).mount('#app')
